51a5375ea938e825619eb9b824e3d931db557c51
JianyeZhu <947848429@qq.com>
2021-08-30 20:37:03 +0800

    update

--- info.lua
+++ info.lua
@@ -1,8 +1,8 @@
 PluginInfo = {
   Name = "Allen & Heath~Qu Mixer",
   Version = "1.0",
-  BuildVersion = "1.0.0.139",
+  BuildVersion = "1.0.0.140",
   Id = "5b589c6d-5137-492a-a163-2bbfd2f949d4",
   Author = "Johnny Zhu",
-  Description = "Johnny's first plugin"  
+  Description = "It is a one-way communication plugin of controlling a Allen&Heath QU mixer."  
 }
